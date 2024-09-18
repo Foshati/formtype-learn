eadd84f0d5fa5d0c1aefabe527047af78f4e244c
Foshati <amirrezafoshati@gmail.com>
2024-09-18 18:13:52 +0330

    formtype-learn add user

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import { v4 as uuidv4 } from 'uuid';
+
+type ItemProps={
+  id:string
+  name:string
+  email:string
+}
+
+const name=document.querySelector<HTMLInputElement>("#name")
+const email =document.querySelector<HTMLInputElement>("#email")
+const userForm=document.querySelector<HTMLFormElement>("#userForm")
+const userList =document.querySelector<HTMLDivElement>("#userList")
+
+
+userForm?.addEventListener('submit', (e)=>{
+  e.preventDefault()
+
+    if (!name?.value.trim() || !email?.value.trim()) return  /* undefined */
+
+
+
+  const userItem:ItemProps={
+    id:uuidv4(),
+    name:name?.value,
+    email:email?.value,
+  }
+
+  console.log(userItem)
+
+  addUser(userItem)
+    // Reset form fields
+    name.value = ''
+    email.value = ''
+
+})
+
+
+function addUser (item:ItemProps){
+  const container= document.createElement("div")
+  const idElement= document.createElement("p")
+  const nameElement= document.createElement("p")
+  const emailElement= document.createElement("p")
+
+idElement.append(item.id)   /* idElement.textContent = item.id */
+nameElement.append(item.name)
+emailElement.append(item.email)
+
+
+
+
+
+container.append(idElement,nameElement,emailElement)
+userList?.append(container)
+
+}
