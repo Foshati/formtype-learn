2136f94decb875812e32c5c59e221ede8c80fc07
Foshati <amirrezafoshati@gmail.com>
2024-09-19 23:48:34 +0330

    add data and save localStorage

--- src/main.ts
+++ src/main.ts
@@ -1,64 +1,68 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-type ItemProps={
-  id:string
-  name:string
-  email:string
+type ItemProps = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const name = document.querySelector<HTMLInputElement>("#name");
+const email = document.querySelector<HTMLInputElement>("#email");
+const userForm = document.querySelector<HTMLFormElement>("#userForm");
+const userList = document.querySelector<HTMLDivElement>("#userList");
+
+const dataList:ItemProps[]= loadData()
+dataList.forEach(addUser)
+
+
+userForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!name?.value.trim() || !email?.value.trim()) return; /* undefined */
+
+  const userItem: ItemProps = {
+    id: uuidv4(),
+    name: name?.value,
+    email: email?.value,
+  };
+
+  console.log(userItem);
+
+  dataList.push(userItem)
+
+  addUser(userItem);
+
+
+  saveData()
+
+  name.value = "";
+  email.value = "";
+});
+
+function addUser(item: ItemProps) {
+  const container = document.createElement("div");
+  const idElement = document.createElement("p");
+  const nameElement = document.createElement("p");
+  const emailElement = document.createElement("p");
+
+  idElement.append(item.id); /* idElement.textContent = item.id */
+  nameElement.append(item.name);
+  emailElement.append(item.email);
+
+  container.append(idElement, nameElement, emailElement);
+  userList?.append(container);
 }
 
-const name=document.querySelector<HTMLInputElement>("#name")
-const email =document.querySelector<HTMLInputElement>("#email")
-const userForm=document.querySelector<HTMLFormElement>("#userForm")
-const userList =document.querySelector<HTMLDivElement>("#userList")
-// const errorDiv = document.querySelector<HTMLDivElement>("#error")
 
 
-userForm?.addEventListener('submit', (e)=>{
-  e.preventDefault()
+function saveData(){
 
-  // if (name?.value === undefined || email?.value === undefined) return
-  // if (!name?.value.trim() || !email?.value.trim()) {
-  //   errorDiv!.textContent = "Name and email cannot be empty."
-  //   return
-  // } else {
-  //   errorDiv!.textContent = ""
-  // }
+localStorage.setItem("items", JSON.stringify(dataList))
+}
 
-    if (!name?.value.trim() || !email?.value.trim()) return  /* undefined */
+function loadData(){
+  const data=localStorage.getItem("items")
+  if (data == null) return []
 
-
-
-  const userItem:ItemProps={
-    id:uuidv4(),
-    name:name?.value,
-    email:email?.value,
-  }
-
-  console.log(userItem)
-
-  addUser(userItem)
-    // Reset form fields
-    name.value = ''
-    email.value = ''
-
-})
-
-
-function addUser (item:ItemProps){
-  const container= document.createElement("div")
-  const idElement= document.createElement("p")
-  const nameElement= document.createElement("p")
-  const emailElement= document.createElement("p")
-
-idElement.append(item.id)   /* idElement.textContent = item.id */
-nameElement.append(item.name)
-emailElement.append(item.email)
-
-
-
-
-
-container.append(idElement,nameElement,emailElement)
-userList?.append(container)
-
+return JSON.parse(data)
 }
